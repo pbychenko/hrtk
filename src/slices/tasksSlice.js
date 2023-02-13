d1e95c064113e9f77ad05dc3b62c6a8985680f07
Pavel Bychenko <pavelv87@gmail.com>
2023-02-13 15:18:14 +0600

    add l1

---
+++ src/slices/tasksSlice.js
@@ -0,0 +1,24 @@
+// @ts-check
+/* eslint-disable no-param-reassign */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  tasks: [],
+};
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, { payload: { task } }) => {
+      // BEGIN (write your solution here)
+      state.tasks = [task, ...state.tasks];
+      // END
+    },
+  },
+});
+
+export const { addTask } = tasksSlice.actions;
+
+export default tasksSlice.reducer;
